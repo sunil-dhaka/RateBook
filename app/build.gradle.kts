9d91bf67eae7be1be65f3528cb64e15f8e32b38d
Sunil Dhaka <sunil.dhaka.iitk@gmail.com>
2026-04-23 16:12:24 +0530

    Enable R8 minification and resource shrinking for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
